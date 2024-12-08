78dba10801a140c3002b30acf3174b0566419708
Eu Bra Bo48 <nicknicolas025@gmail.com>
2024-12-08 16:23:26 -0300

    treminei os dialogos

--- projetogamejam/EuBraBo/Script/dialog.gd
+++ projetogamejam/EuBraBo/Script/dialog.gd
@@ -9,7 +9,7 @@ var data: Dictionary = {}
 @export_category("Objects")
 @export var _name: Label = null
 @export var _texte: RichTextLabel = null
-@export var _icone: Sprite2D  = null
+@export var _icone: TextureRect  = null
 
 
 func _ready() -> void:
@@ -33,8 +33,7 @@ func _initialice_dialog() -> void:
 	if data.has(_id):
 		_name.text = data[_id]["title"]
 		_texte.text = data[_id]["dialog"]
-		#_icone
-		_texte.visible_characters = 0
+		_icone.texture = load(data[_id]["icone"])
 		
 		while _texte.visible_ratio < 1:
 			await get_tree().create_timer(_speed).timeout
---
+++ projetogamejam/EuBraBo/Script/dialog_principal.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+const DIALOG_SCREEEN: PackedScene = preload("res://EuBraBo/Scenes/dialog.tscn")
+
+var DialogPrincipalDia1: Dictionary = {
+	0: {"dialog": "Kkokkko... Son... Kokkko...", 
+		"title": "Helen", 
+		"icone": "res://CoisaEmGeral/icon.svg"
+	},
+	1: {"dialog": "Mom! Mom! You’re having another coughing fit! My God, I need to get a job to pay for your surgery!", 
+		"title": "Ethan", 
+		"icone": "res://CoisaEmGeral/icon.svg"
+	},
+	2: {"dialog": "My dear... Kokkko... You don’t have to... But if you do work, take your phone. I’ll call you during your lunch break, my son.", 
+		"title": "Helen", 
+		"icone": "res://CoisaEmGeral/icon.svg"
+	},
+	3: {"dialog": "(crying) Yes, mom...", 
+		"title": "Ethan", 
+		"icone": "res://CoisaEmGeral/icon.svg"
+	}
+}
+
+@export_category("Objects")
+@export var hud: CanvasLayer = null
+
+
+func _ready() -> void:
+	var dialog: DialogScren = DIALOG_SCREEEN.instantiate()
+	dialog.data = DialogPrincipalDia1
+	hud.add_child(dialog)
